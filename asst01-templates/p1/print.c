5ed91696a55faff4009d8d57775f11d2377a92b0
David Bumm <64277116+u-Texon@users.noreply.github.com>
2020-11-10 09:52:13 +0100

    added templats from ilias for the first assignment

---
+++ asst01-templates/p1/print.c
@@ -0,0 +1,8 @@
+#include "print.h"
+
+void print_line(int64_t number, char *string)
+{
+    (void) number;
+    (void) string;
+    // Add code here.
+}
---
+++ asst01-templates/p2/parseint.c
@@ -0,0 +1,22 @@
+#include "parseint.h"
+
+/*
+ * Returns the value of c or -1 on error
+ */
+int parseDecimalChar(char c)
+{
+    (void)c;
+
+    return -1;
+}
+
+/*
+ * Parses a non-negative integer, interpreted as octal when starting with 0,
+ * decimal otherwise. Returns -1 on error.
+ */
+int parseInt(char *string)
+{
+    (void)string;
+
+    return -1;
+}
